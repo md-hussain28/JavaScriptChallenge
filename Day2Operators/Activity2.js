bf1baf9695e774f3b96a69a750e2dcc6b6c2a9d8
md-hussain28 <mdsaquib28official@gmail.com>
2024-07-15 16:02:39 +0530

    Day2

---
+++ Day2Operators/Activity2.js
@@ -0,0 +1,40 @@
+//Activity 1:
+let a=50,b=40;
+
+console.log("Addition: ",a+b);
+console.log("Sunstraction ",a-b);
+console.log("Multiply :",a*b);
+console.log("Divide :",a/b);
+console.log("Remainder :",a%b);
+
+//Activity 2:
+a=10; 
+a+=5;
+console.log("Use of a+=5 :",a);
+a-=8;
+console.log("Use of a-=8 :",a);
+
+//Activity 3:
+a=8; b=10;
+console.log("a > b",a>b);
+console.log("a < b",a<b);
+
+a=9; b=9;
+console.log("a <= b",a<=b);
+console.log("a >= b",a>=b);
+
+a=10; b="10";
+console.log("a == b",a==b);
+console.log("a === b",a===b);
+
+a=6; b=10;
+console.log("a>0 and b>0 :", a>0&&b>0);
+console.log("a>8 or b>8 :", a>8||b>8);
+console.log("If 8 not equal to 6 :",a!=8);
+
+//Activity 5:
+a=10; 
+let sign=a>0?"Positive":"Negative";
+console.log("Value of a is :",sign);
+
+
